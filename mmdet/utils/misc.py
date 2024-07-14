a0f78fe9a5f5d99d55d3ee9e573a2bb396bea33c
ironmanfcf <2399642395@qq.com>
2024-07-14 20:49:05 +0800

    utils

--- mmdet/utils/misc.py
+++ mmdet/utils/misc.py
@@ -6,6 +6,7 @@ import urllib
 import warnings
 from typing import Union
 
+import numpy as np
 import torch
 from mmengine.config import Config, ConfigDict
 from mmengine.logging import print_log
@@ -13,6 +14,38 @@ from mmengine.utils import scandir
 
 IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif',
                   '.tiff', '.webp')
+
+def auto_arrange_images(image_list: list, image_column: int = 2) -> np.ndarray:
+    """Auto arrange image to image_column x N row.
+
+    Args:
+        image_list (list): cv2 image list.
+        image_column (int): Arrange to N column. Default: 2.
+    Return:
+        (np.ndarray): image_column x N row merge image
+    """
+    img_count = len(image_list)
+    if img_count <= image_column:
+        # no need to arrange
+        image_show = np.concatenate(image_list, axis=1)
+    else:
+        # arrange image according to image_column
+        image_row = round(img_count / image_column)
+        fill_img_list = [np.ones(image_list[0].shape, dtype=np.uint8) * 255
+                         ] * (
+                             image_row * image_column - img_count)
+        image_list.extend(fill_img_list)
+        merge_imgs_col = []
+        for i in range(image_row):
+            start_col = image_column * i
+            end_col = image_column * (i + 1)
+            merge_col = np.hstack(image_list[start_col:end_col])
+            merge_imgs_col.append(merge_col)
+
+        # merge to one image
+        image_show = np.vstack(merge_imgs_col)
+
+    return image_show
 
 
 def find_latest_checkpoint(path, suffix='pth'):
